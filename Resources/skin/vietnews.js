f9753b779633a34c4983ff6abfa5c42cca6a5f75
Leon Truong <leontruong@gmail.com>
2013-06-24 11:01:23 -0500

    MASTER JEDI!

---
+++ Resources/skin/vietnews.js
@@ -0,0 +1,84 @@
+var skin = {
+
+    // recent news title bar color and image
+    // if use image, bar color and title should empty
+    // if use text title, bar image should empty
+    "RECENT_BAR_IMAGE"     : "",        // example: imgs/wpapp.png, size: 320x45
+    "RECENT_BAR_COLOR"     : "#1d1d1d", // example: #ff840a
+
+    // category title bar color and image
+    // if use image, bar color and title should empty
+    // if use text title, bar image should empty
+    "CATEG_BAR_IMAGE"     : "",        // example: imgs/wpapp.png, size: 320x45
+    "CATEG_BAR_COLOR"     : "#1d1d1d", // example: #ff840a
+    "CATEG_DASHBOARD_BGCOLOR" : "#f0efeb",
+
+    // blog post title bar color and image
+    // if use image, bar color should empty
+    // if use text title, bar image should empty
+    "POST_BAR_IMAGE"      : "",        // example: imgs/wpapp.png, size: 320x45
+    "POST_BAR_COLOR"      : "#1d1d1d", // example: #ff840a
+
+    // about tab bar color and image
+    // if use image, bar color and title should empty
+    // if use text title, bar image should empty
+    "ABOUT_BAR_IMAGE"     : "",        // example: imgs/wpapp.png, size: 320x45
+    "ABOUT_BAR_COLOR"     : "#1d1d1d", // example: #ff840a
+
+    "WEBVIEW_BAR_IMAGE"   : "",        // example: imgs/wpapp.png, size: 320x45
+    "WEBVIEW_BAR_COLOR"   : "#1d1d1d", // example: #ff840a
+    
+    "TWITTER_BAR_IMAGE"   : "",        // example: imgs/wpapp.png, size: 320x45
+    "TWITTER_BAR_COLOR"   : "#1d1d1d", // example: #ff840a
+    
+    "PAGES_BAR_IMAGE"     : "",        // example: imgs/wpapp.png, size: 320x45
+    "PAGES_BAR_COLOR"     : "#1d1d1d", // example: #ff840a
+
+
+
+    // table view for recent news
+    "RECENT_TV_BGCOLOR"    : "#eceae5",   
+    "RECENT_TV_BGCOLOR_ALT": "#eceae5",
+    "RECENT_TV_SEPARATOR_COLOR": "#d7d5d0",
+    "RECENT_TV_TITLE_COLOR": "#444444", //List Title Color   
+    "RECENT_TV_META_COLOR" : "#a6a6a6",  
+
+
+    // table view for categories 
+    "CATEG_TV_BGCOLOR"    : "#eceae5",   
+    "CATEG_TV_BGCOLOR_ALT": "#eceae5",   
+    "CATEG_TV_SEPARATOR_COLOR": "#d7d5d0",  
+    "CATEG_TV_TITLE_COLOR": "#444444",   
+    "CATEG_TV_META_COLOR" : "#a6a6a6",  
+    
+    // table view for twitter 
+    "TWITTER_TV_BGCOLOR"    : "#000000",   
+    "TWITTER_TV_BGCOLOR_ALT": "#3f3f3f",   
+    "TWITTER_TV_SEPARATOR_COLOR": "#d7d5d0",   
+    "TWITTER_TV_TITLE_COLOR": "#444444",   
+    "TWITTER_TV_META_COLOR" : "#a6a6a6", 
+    
+    // table view for pages 
+    "PAGES_TV_BGCOLOR"    : "#000000",   
+    "PAGES_TV_BGCOLOR_ALT": "#3f3f3f",   
+    "PAGES_TV_TITLE_COLOR": "#444444", 
+
+    "DUMMY" : ""   
+};
+
+// skin.RECENT_TV_GRADIENT = {
+//   type:'linear',
+//   colors:[{color:'#d4d4d4',position:0.0},
+//     {color:'#c4c4c4',position:0.50},{color:'#b4b4b4',position:1.0}
+//   ]};
+
+// skin.CATEG_TV_GRADIENT = {
+//   type:'linear',
+//   colors:[{color:'#d4d4d4',position:0.0},
+//     {color:'#c4c4c4',position:0.50},{color:'#b4b4b4',position:1.0}
+//   ]};
+
+// skin.TWITTER_TV_GRADIENT = {
+  // type:'linear',
+  // colors:[ {color:'#404040',position:0.0},{color:'#2f2f2f',position:0.5},{color:'#272727',position:1.0} ]
+// };
